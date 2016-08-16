b32580f69a0d6d6f675263255bd994d7a1c0af6b
Ian Hansen <ihansen@digitalocean.com>
2016-08-16 11:27:19 -0700

    fixes linting errors

--- storage/resolver.go
+++ storage/resolver.go
@@ -2,15 +2,23 @@ package storage
 
 import "github.com/digitalocean/vulcan/bus"
 
+// MatchType is an enum representing a PromQL label match from "=", "!=", "=~"
+// or "!~"
 type MatchType int
 
 const (
+	// Equal is "=" in PromQL
 	Equal MatchType = iota
+	// NotEqual is "!=" in PromQL
 	NotEqual
+	// RegexMatch is "=~" in PromQL
 	RegexMatch
+	// RegexNoMatch is "!~" in PromQL
 	RegexNoMatch
 )
 
+// Match represents the PromQL matching function and on what label name to
+// operate.
 type Match struct {
 	Type  MatchType
 	Name  string
