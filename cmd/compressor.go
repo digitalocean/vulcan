d76edbcfe6c72157254eb83637006cdf6882a928
Ian Hansen <ian@supershabam.com>
2016-11-22 02:44:29 -0700

    a quick pass through; not finished

---
+++ cmd/compressor.go
@@ -0,0 +1,157 @@
+// Copyright 2016 The Vulcan Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/Sirupsen/logrus"
+	"github.com/digitalocean/vulcan/compressor"
+	"github.com/gocql/gocql"
+	hostpool "github.com/hailocab/go-hostpool"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	cg "github.com/supershabam/sarama-cg"
+	"github.com/supershabam/sarama-cg/protocol"
+)
+
+func Compressor() *cobra.Command {
+	cmpr := &cobra.Command{
+		Use:   "compressor",
+		Short: "compressor reads datapoints from the bus and persists a compressed blob of samples",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, cancel := context.WithCancel(context.Background())
+			term := make(chan os.Signal, 1)
+			signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				<-term
+				logrus.Info("shutting down...")
+				cancel()
+				<-term
+				os.Exit(1)
+			}()
+
+			cassandraAddrs := strings.Split(viper.GetString(flagCassandraAddrs), ",")
+			cassandraKeyspace := viper.GetString(flagCassandraKeyspace)
+			cassandraTimeout := viper.GetDuration(flagCassandraTimeout)
+			cassandraConn := viper.GetInt(flagCassandraNumConns)
+			webListenAddr := viper.GetString(flagWebListenAddress)
+			kafkaAddrs := strings.Split(viper.GetString(flagKafkaAddrs), ",")
+			clientID := viper.GetString(flagKafkaClientID)
+			groupID := viper.GetString(flagKafkaGroupID)
+			kafkaSessionTimeout := viper.GetDuration(flagKafkaSession)
+			kafkaHeartbeat := viper.GetDuration(flagKafkaHeartbeat)
+			kafkaTopic := viper.GetString(flagKafkaTopic)
+
+			logrus.WithFields(logrus.Fields{
+				"web_listen_addr":       webListenAddr,
+				"kafka_addrs":           kafkaAddrs,
+				"kafka_client_id":       clientID,
+				"kafka_group_id":        groupID,
+				"kafka_heartbeat":       kafkaHeartbeat,
+				"kafka_session_timeout": kafkaSessionTimeout,
+				"kafka_topic":           kafkaTopic,
+				"cassandra_addrs":       cassandraAddrs,
+				"cassandra_keyspace":    cassandraKeyspace,
+				"cassandra_timeout":     cassandraTimeout,
+			}).Info("starting cacher")
+
+			// Setup Cassandra writer
+			cluster := gocql.NewCluster(cassandraAddrs...)
+			cluster.Keyspace = cassandraKeyspace
+			cluster.Timeout = cassandraTimeout
+			cluster.NumConns = cassandraConn
+			cluster.Consistency = gocql.LocalOne
+			cluster.ProtoVersion = 4
+			// Fallback simple host pool distributes queries and prevents sending queries to unresponsive hosts.
+			fallbackHostPolicy := gocql.HostPoolHostPolicy(hostpool.New(nil))
+			cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(fallbackHostPolicy)
+			sess, err := cluster.CreateSession()
+			if err != nil {
+				return err
+			}
+			cfg := sarama.NewConfig()
+			cfg.Version = sarama.V0_10_0_0
+			cfg.ClientID = clientID
+			client, err := sarama.NewClient(kafkaAddrs, cfg)
+			if err != nil {
+				return err
+			}
+			coord := cg.NewCoordinator(&cg.CoordinatorConfig{
+				Client:  client,
+				Context: ctx,
+				GroupID: groupID,
+				Protocols: []cg.ProtocolKey{
+					{
+						Protocol: &protocol.RoundRobin{},
+						Key:      "roundrobin",
+					},
+				},
+				SessionTimeout: kafkaSessionTimeout,
+				Heartbeat:      kafkaHeartbeat,
+				Topics:         []string{kafkaTopic},
+			})
+			c, err := compressor.NewCompressor(&compressor.CompressorConfig{
+				Client:      client,
+				Coordinator: coord,
+				GroupID:     groupID,
+				Session:     sess,
+				TTL:         time.Hour * 24 * 3,
+			})
+			prometheus.MustRegister(c)
+			var outerErr error
+			// run http server in goroutine and allow it to close context and record error if any.
+			go func() {
+				defer cancel()
+				http.Handle("/metrics", prometheus.Handler())
+				err := http.ListenAndServe(webListenAddr, nil)
+				if err != nil {
+					outerErr = err
+				}
+			}()
+			// run cacher service until it's done (will stop when context is canceled)
+			err = c.Run()
+			if err != nil {
+				// if cacher failed, return its error
+				return err
+			}
+			// otherwise, the cacher stopped because the context canceled because of grpc/http error or shutdown.
+			return outerErr
+		},
+	}
+
+	cmpr.Flags().Duration(flagCassandraTimeout, time.Second*2, "cassandra timeout duration")
+	cmpr.Flags().Int(flagCassandraNumConns, 5, "number of connections to cassandra per node")
+	cmpr.Flags().Int(flagNumCassandraWorkers, 200, "number of ingester goroutines to write to cassandra")
+	cmpr.Flags().String(flagCassandraAddrs, "", "one.example.com:9092,two.example.com:9092")
+	cmpr.Flags().String(flagCassandraKeyspace, "vulcan", "cassandra keyspace to use")
+	cmpr.Flags().String(flagWebListenAddress, ":8080", "web address for telemetry")
+	cmpr.Flags().String(flagKafkaAddrs, "", "one.example.com:9092,two.example.com:9092")
+	cmpr.Flags().String(flagKafkaClientID, "vulcan-cacher", "set the kafka client id")
+	cmpr.Flags().String(flagKafkaGroupID, "vulcan-cacher", "workers with the same groupID will join the same Kafka ConsumerGroup")
+	cmpr.Flags().Duration(flagKafkaHeartbeat, time.Second*3, "kafka consumer group heartbeat interval")
+	cmpr.Flags().Duration(flagKafkaSession, time.Second*30, "kafka consumer group session duration")
+	cmpr.Flags().String(flagKafkaTopic, "vulcan", "set the kafka topic to consume")
+
+	return cmpr
+}
---
+++ compressor/compressor.go
@@ -0,0 +1,338 @@
+// Copyright 2016 The Vulcan Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compressor
+
+import (
+	"context"
+	"math"
+	"strconv"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/Sirupsen/logrus"
+	"github.com/digitalocean/vulcan/model"
+	"github.com/gocql/gocql"
+	"github.com/golang/protobuf/proto"
+	"github.com/prometheus/client_golang/prometheus"
+	pmodel "github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/storage/local/chunk"
+	"github.com/prometheus/prometheus/storage/remote"
+	cg "github.com/supershabam/sarama-cg"
+	"github.com/supershabam/sarama-cg/consumer"
+)
+
+type CompressorConfig struct {
+	Client      sarama.Client
+	Coordinator *cg.Coordinator
+	GroupID     string
+	MaxAge      time.Duration
+	MaxIdle     time.Duration
+	Session     *gocql.Session
+	TTL         time.Duration
+}
+
+type Compressor struct {
+	cfg              *CompressorConfig
+	ttl              int64 // time to live in seconds.
+	flushUtilization prometheus.Summary
+	highwatermark    *prometheus.GaugeVec
+	offset           *prometheus.GaugeVec
+	samplesTotal     *prometheus.CounterVec
+	writeDuration    prometheus.Summary
+}
+
+func NewCompressor(cfg *CompressorConfig) (*Compressor, error) {
+	return &Compressor{
+		cfg: cfg,
+		ttl: int64(cfg.TTL.Seconds()),
+		flushUtilization: prometheus.NewSummary(prometheus.SummaryOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "flush_utilization",
+			Help:      "Summary of utilization percentage 0-1 of a flushed chunk",
+			Objectives: map[float64]float64{
+				0.1: 0.01,
+				0.5: 0.05,
+				0.9: 0.01,
+			},
+		}),
+		highwatermark: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "highwatermark",
+			Help:      "Current highwatermark of a topic-partition",
+		}, []string{"topic", "partition"}),
+		offset: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "offset",
+			Help:      "Current offset of a topic-partition",
+		}, []string{"topic", "partition"}),
+		samplesTotal: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "samples_total",
+			Help:      "count of samples ingested into compressor",
+		}, []string{"topic", "partition"}),
+		writeDuration: prometheus.NewSummary(prometheus.SummaryOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "write_duration_seconds",
+			Help:      "Summary of durations taken writing a chunk to the database",
+		}),
+	}, nil
+}
+
+// Describe implements prometheus.Collector.
+func (c *Compressor) Describe(ch chan<- *prometheus.Desc) {
+	c.flushUtilization.Describe(ch)
+	c.highwatermark.Describe(ch)
+	c.offset.Describe(ch)
+	c.samplesTotal.Describe(ch)
+	c.writeDuration.Describe(ch)
+}
+
+// Collect implements prometheus.Collector.
+func (c *Compressor) Collect(ch chan<- prometheus.Metric) {
+	c.highwatermark.Collect(ch)
+	c.flushUtilization.Collect(ch)
+	c.offset.Collect(ch)
+	c.samplesTotal.Collect(ch)
+	c.writeDuration.Collect(ch)
+}
+
+func (c *Compressor) Run() error {
+	return c.cfg.Coordinator.Run(c.handle)
+}
+
+func (c *Compressor) handle(ctx context.Context, topic string, partition int32) {
+	if partition != 2 {
+		return
+	}
+	log := logrus.WithFields(logrus.Fields{
+		"topic":     topic,
+		"partition": partition,
+	})
+	log.Info("taking control of topic-partition")
+	count := 0
+	backoff := time.NewTimer(time.Duration(0))
+	for {
+		count++
+		select {
+		case <-ctx.Done():
+			return
+		case <-backoff.C:
+			err := c.consume(ctx, topic, partition)
+			if err == nil {
+				logrus.WithFields(logrus.Fields{
+					"topic":     topic,
+					"partition": partition,
+				}).Info("relenquishing control of topic-partition")
+				return
+			}
+			// exponential backoff with cap at 10m
+			dur := time.Duration(math.Min(float64(time.Minute*10), float64(100*time.Millisecond)*math.Pow(float64(2), float64(count))))
+			logrus.WithFields(logrus.Fields{
+				"backoff_duration": dur,
+				"topic":            topic,
+				"partition":        partition,
+			}).WithError(err).Error("error while consuming but restarting after backoff")
+			backoff.Reset(dur)
+		}
+	}
+}
+
+func (c *Compressor) consume(ctx context.Context, topic string, partition int32) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel() // ensure consumer context is canceled nomatter why we exit this function.
+	partitionStr := strconv.FormatInt(int64(partition), 10)
+	defer func() {
+		c.highwatermark.DeleteLabelValues(topic, partitionStr)
+		c.offset.DeleteLabelValues(topic, partitionStr)
+		c.samplesTotal.DeleteLabelValues(topic, partitionStr)
+	}()
+	cr, err := consumer.NewSeek(&consumer.SeekConfig{
+		CacheDuration: time.Second,
+		Client:        c.cfg.Client,
+		Coordinator:   c.cfg.Coordinator,
+		Context:       ctx,
+		Partition:     partition,
+		SeekFn:        c.seek,
+		Topic:         topic,
+	})
+	if err != nil {
+		return err
+	}
+	// TODO: expire elements in acc that haven't received an update. - idle
+	// TODO: expire chunks that have values that are too old. - maxAge
+	chunks := map[string]chunk.Chunk{}
+	// iterate through messages from this partition in-order committing the offset upon completion.
+	for msg := range cr.Consume() {
+		c.highwatermark.WithLabelValues(topic, partitionStr).Set(float64(cr.HighWaterMarkOffset()))
+		c.offset.WithLabelValues(topic, partitionStr).Set(float64(msg.Offset))
+		flush := map[string][]chunk.Chunk{}
+		tsb, err := parseTimeSeriesBatch(msg.Value)
+		if err != nil {
+			return err
+		}
+		for _, ts := range tsb {
+			id := ts.ID()
+			if _, ok := chunks[id]; !ok {
+				chnk, err := chunk.NewForEncoding(chunk.Varbit)
+				if err != nil {
+					return err
+				}
+				chunks[id] = chnk
+			}
+			for _, s := range ts.Samples {
+				next, err := chunks[id].Add(pmodel.SamplePair{
+					Timestamp: pmodel.Time(s.TimestampMS),
+					Value:     pmodel.SampleValue(s.Value),
+				})
+				if err != nil {
+					return err
+				}
+				// if added sample to chunk without overflow
+				if len(next) == 1 {
+					chunks[id] = next[0]
+					continue
+				}
+				if len(next) != 2 {
+					panic("appending a sample to chunk should always result in a 1 or 2 element slice")
+				}
+				// chunk overflowed and we need to flush the full chunk.
+				if _, ok := flush[id]; !ok {
+					flush[id] = make([]chunk.Chunk, 0, 1)
+				}
+				flush[id] = append(flush[id], next[0])
+				// write overflow samples to new varbit chunk
+				iter := next[1].NewIterator()
+				chnk, err := chunk.NewForEncoding(chunk.Varbit)
+				if err != nil {
+					return err
+				}
+				next[0] = chnk
+				for iter.Scan() {
+					n, err := next[0].Add(iter.Value())
+					if err != nil {
+						return err
+					}
+					if len(n) != 1 {
+						panic("expected overflow samples to fit into new varbit chunk")
+					}
+					next = n
+				}
+			}
+			c.samplesTotal.WithLabelValues(topic, partitionStr).Add(float64(len(ts.Samples)))
+		}
+		// TODO keep to-flush in memory and don't call commit offset each time; instead call commit offset
+		// once the in-memory buffer has been flushed (but not on each message read)
+		err = c.flush(flush)
+		if err != nil {
+			return err
+		}
+		err = cr.CommitOffset(msg.Offset)
+		if err != nil {
+			return err
+		}
+	}
+	return cr.Err()
+}
+
+func (c *Compressor) flush(flush map[string][]chunk.Chunk) error {
+	for id, chunks := range flush {
+		for _, chnk := range chunks {
+			err := c.write(id, chnk)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Compressor) seek(topic string, partition int32) (int64, error) {
+	b, err := c.cfg.Client.Coordinator(c.cfg.GroupID)
+	if err != nil {
+		return 0, err
+	}
+	req := &sarama.OffsetFetchRequest{
+		ConsumerGroup: c.cfg.GroupID,
+		Version:       1,
+	}
+	req.AddPartition(topic, partition)
+	resp, err := b.FetchOffset(req)
+	if err != nil {
+		return 0, err
+	}
+	blk := resp.GetBlock(topic, partition)
+	if blk.Err != sarama.ErrNoError {
+		return 0, blk.Err
+	}
+	logrus.WithField("offset", blk.Offset).Info("seek to offset")
+	if blk.Offset < 0 {
+		return c.cfg.Client.GetOffset(topic, partition, blk.Offset)
+	}
+	return blk.Offset, nil
+}
+
+func (c *Compressor) write(id string, chnk chunk.Chunk) error {
+	end, err := c.lastTime(chnk)
+	if err != nil {
+		return err
+	}
+	var buf []byte
+	err = chnk.MarshalToBuf(buf)
+	if err != nil {
+		return err
+	}
+	sql := `UPDATE compressed USING TTL ? SET value = ? WHERE id = ? AND end = ?`
+	c.flushUtilization.Observe(chnk.Utilization())
+	t0 := time.Now()
+	err = c.cfg.Session.Query(sql, c.ttl, buf, id, end).Exec()
+	c.writeDuration.Observe(time.Since(t0).Seconds())
+	return err
+}
+
+func (c *Compressor) lastTime(chnk chunk.Chunk) (int64, error) {
+	iter := chnk.NewIterator()
+	t, err := iter.LastTimestamp()
+	return int64(t), err
+}
+
+func parseTimeSeriesBatch(in []byte) (model.TimeSeriesBatch, error) {
+	wr := &remote.WriteRequest{}
+	if err := proto.Unmarshal(in, wr); err != nil {
+		return nil, err
+	}
+	tsb := make(model.TimeSeriesBatch, 0, len(wr.Timeseries))
+	for _, protots := range wr.Timeseries {
+		ts := &model.TimeSeries{
+			Labels:  map[string]string{},
+			Samples: make([]*model.Sample, 0, len(protots.Samples)),
+		}
+		for _, pair := range protots.Labels {
+			ts.Labels[pair.Name] = pair.Value
+		}
+		for _, protosamp := range protots.Samples {
+			ts.Samples = append(ts.Samples, &model.Sample{
+				TimestampMS: protosamp.TimestampMs,
+				Value:       protosamp.Value,
+			})
+		}
+		tsb = append(tsb, ts)
+	}
+	return tsb, nil
+}
