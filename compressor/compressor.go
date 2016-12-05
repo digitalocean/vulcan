23f6cfc5e6bace677507fb3d4b03feee5787302b
Ian Hansen <ian@supershabam.com>
2016-12-05 09:00:44 -0800

    wip wip

--- compressor/compressor.go
+++ compressor/compressor.go
@@ -16,161 +16,34 @@ package compressor
 
 import (
 	"context"
-	"math"
 	"strconv"
 	"sync"
 	"time"
 
-	"github.com/Shopify/sarama"
-	"github.com/Sirupsen/logrus"
 	"github.com/digitalocean/vulcan/model"
 	"github.com/gocql/gocql"
 	"github.com/golang/protobuf/proto"
-	"github.com/prometheus/client_golang/prometheus"
-	pmodel "github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/storage/local/chunk"
 	"github.com/prometheus/prometheus/storage/remote"
-	cg "github.com/supershabam/sarama-cg"
 	"github.com/supershabam/sarama-cg/consumer"
 )
 
 type CompressorConfig struct {
-	Client      sarama.Client
-	Coordinator *cg.Coordinator
-	GroupID     string
-	MaxAge      time.Duration
-	MaxIdle     time.Duration
-	Session     *gocql.Session
-	TTL         time.Duration
+	Context         context.Context
+	Consumer        Consumer
+	LastTimestamper LastTimestamper
 }
 
+// Compressor is able to take a stream of metrics and flush them in a compressed format.
+// It aims to be resumable.
 type Compressor struct {
-	cfg              *CompressorConfig
-	maxAge           int64 // max age in milliseconds
-	maxIdle          int64 // max idle in milliseconds
-	ttl              int64 // time to live in seconds.
-	fetchDuration    prometheus.Summary
-	flushUtilization prometheus.Summary
-	highwatermark    *prometheus.GaugeVec
-	offset           *prometheus.GaugeVec
-	samplesTotal     *prometheus.CounterVec
-	writeDuration    prometheus.Summary
-}
-
-func NewCompressor(cfg *CompressorConfig) (*Compressor, error) {
-	return &Compressor{
-		cfg:     cfg,
-		maxAge:  int64(cfg.MaxAge.Seconds()),
-		maxIdle: int64(cfg.MaxIdle.Seconds()),
-		ttl:     int64(cfg.TTL.Seconds()),
-		fetchDuration: prometheus.NewSummary(prometheus.SummaryOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "fetch_duration_seconds",
-			Help:      "Summary of durations taken getting end time from database",
-		}),
-		flushUtilization: prometheus.NewSummary(prometheus.SummaryOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "flush_utilization",
-			Help:      "Summary of utilization percentage 0-1 of a flushed chunk",
-			Objectives: map[float64]float64{
-				0.1: 0.01,
-				0.5: 0.05,
-				0.9: 0.01,
-			},
-		}),
-		highwatermark: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "highwatermark",
-			Help:      "Current highwatermark of a topic-partition",
-		}, []string{"topic", "partition"}),
-		offset: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "offset",
-			Help:      "Current offset of a topic-partition",
-		}, []string{"topic", "partition"}),
-		samplesTotal: prometheus.NewCounterVec(prometheus.CounterOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "samples_total",
-			Help:      "count of samples ingested into compressor",
-		}, []string{"topic", "partition"}),
-		writeDuration: prometheus.NewSummary(prometheus.SummaryOpts{
-			Namespace: "vulcan",
-			Subsystem: "compressor",
-			Name:      "write_duration_seconds",
-			Help:      "Summary of durations taken writing a chunk to the database",
-		}),
-	}, nil
-}
-
-// Describe implements prometheus.Collector.
-func (c *Compressor) Describe(ch chan<- *prometheus.Desc) {
-	c.fetchDuration.Describe(ch)
-	c.flushUtilization.Describe(ch)
-	c.highwatermark.Describe(ch)
-	c.offset.Describe(ch)
-	c.samplesTotal.Describe(ch)
-	c.writeDuration.Describe(ch)
-}
-
-// Collect implements prometheus.Collector.
-func (c *Compressor) Collect(ch chan<- prometheus.Metric) {
-	c.fetchDuration.Collect(ch)
-	c.flushUtilization.Collect(ch)
-	c.highwatermark.Collect(ch)
-	c.offset.Collect(ch)
-	c.samplesTotal.Collect(ch)
-	c.writeDuration.Collect(ch)
-}
-
-func (c *Compressor) Run() error {
-	return c.cfg.Coordinator.Run(c.handle)
-}
-
-func (c *Compressor) handle(ctx context.Context, topic string, partition int32) {
-	if partition != 2 {
-		return
-	}
-	log := logrus.WithFields(logrus.Fields{
-		"topic":     topic,
-		"partition": partition,
-	})
-	log.Info("taking control of topic-partition")
-	count := 0
-	backoff := time.NewTimer(time.Duration(0))
-	for {
-		count++
-		select {
-		case <-ctx.Done():
-			return
-		case <-backoff.C:
-			err := c.consume(ctx, topic, partition)
-			if err == nil {
-				logrus.WithFields(logrus.Fields{
-					"topic":     topic,
-					"partition": partition,
-				}).Info("relenquishing control of topic-partition")
-				return
-			}
-			// exponential backoff with cap at 10m
-			dur := time.Duration(math.Min(float64(time.Minute*10), float64(100*time.Millisecond)*math.Pow(float64(2), float64(count))))
-			logrus.WithFields(logrus.Fields{
-				"backoff_duration": dur,
-				"topic":            topic,
-				"partition":        partition,
-			}).WithError(err).Error("error while consuming but restarting after backoff")
-			backoff.Reset(dur)
-		}
-	}
+	cfg *CompressorConfig
 }
 
 type chunkLast struct {
-	Chunk chunk.Chunk
-	MinTS int64
+	Chunk  chunk.Chunk
+	MinTS  int64
+	MinOff int64
 }
 
 func (c *Compressor) consume(ctx context.Context, topic string, partition int32) error {
@@ -182,14 +55,15 @@ func (c *Compressor) consume(ctx context.Context, topic string, partition int32)
 		c.offset.DeleteLabelValues(topic, partitionStr)
 		c.samplesTotal.DeleteLabelValues(topic, partitionStr)
 	}()
-	cr, err := consumer.NewSeek(&consumer.SeekConfig{
+	cr, err := consumer.NewTimeWindow(&consumer.TimeWindowConfig{
 		CacheDuration: time.Second,
 		Client:        c.cfg.Client,
 		Coordinator:   c.cfg.Coordinator,
 		Context:       ctx,
 		Partition:     partition,
-		SeekFn:        c.seek,
+		Start:         consumer.OffsetGroup,
 		Topic:         topic,
+		Window:        c.cfg.MaxAge * 2,
 	})
 	if err != nil {
 		return err
@@ -342,38 +216,13 @@ func (c *Compressor) flush(flush map[string][]chunk.Chunk) error {
 	return nil
 }
 
-func (c *Compressor) seek(topic string, partition int32) (int64, error) {
-	b, err := c.cfg.Client.Coordinator(c.cfg.GroupID)
-	if err != nil {
-		return 0, err
-	}
-	req := &sarama.OffsetFetchRequest{
-		ConsumerGroup: c.cfg.GroupID,
-		Version:       1,
-	}
-	req.AddPartition(topic, partition)
-	resp, err := b.FetchOffset(req)
-	if err != nil {
-		return 0, err
-	}
-	blk := resp.GetBlock(topic, partition)
-	if blk.Err != sarama.ErrNoError {
-		return 0, blk.Err
-	}
-	logrus.WithField("offset", blk.Offset).Info("seek to offset")
-	if blk.Offset < 0 {
-		return c.cfg.Client.GetOffset(topic, partition, blk.Offset)
-	}
-	return blk.Offset, nil
-}
-
 func (c *Compressor) write(id string, chnk chunk.Chunk) error {
 	end, err := chnk.NewIterator().LastTimestamp()
 	if err != nil {
 		return err
 	}
 	// TODO is it possible to marshal a resized and fully utilized chunk? As-is, chunks are always 1024 bytes.
-	var buf []byte
+	buf := make([]byte, chunk.ChunkLen)
 	err = chnk.MarshalToBuf(buf)
 	if err != nil {
 		return err
---
+++ compressor/manager.go
@@ -0,0 +1,167 @@
+// Copyright 2016 The Vulcan Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compressor
+
+import (
+	"context"
+	"math"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/Sirupsen/logrus"
+	"github.com/gocql/gocql"
+	"github.com/prometheus/client_golang/prometheus"
+	cg "github.com/supershabam/sarama-cg"
+)
+
+type ManagerConfig struct {
+	Client      sarama.Client
+	Coordinator *cg.Coordinator
+	GroupID     string
+	MaxAge      time.Duration
+	MaxIdle     time.Duration
+	Session     *gocql.Session
+	TTL         time.Duration
+}
+
+type Manager struct {
+	cfg              *ManagerConfig
+	maxAge           int64 // max age in milliseconds
+	maxIdle          int64 // max idle in milliseconds
+	ttl              int64 // time to live in seconds.
+	fetchDuration    prometheus.Summary
+	flushUtilization prometheus.Summary
+	highwatermark    *prometheus.GaugeVec
+	offset           *prometheus.GaugeVec
+	samplesTotal     *prometheus.CounterVec
+	writeDuration    prometheus.Summary
+}
+
+func NewManager(cfg *ManagerConfig) (*Manager, error) {
+	return &Compressor{
+		cfg:     cfg,
+		maxAge:  int64(cfg.MaxAge.Seconds()),
+		maxIdle: int64(cfg.MaxIdle.Seconds()),
+		ttl:     int64(cfg.TTL.Seconds()),
+		fetchDuration: prometheus.NewSummary(prometheus.SummaryOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "fetch_duration_seconds",
+			Help:      "Summary of durations taken getting end time from database",
+		}),
+		flushUtilization: prometheus.NewSummary(prometheus.SummaryOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "flush_utilization",
+			Help:      "Summary of utilization percentage 0-1 of a flushed chunk",
+			Objectives: map[float64]float64{
+				0.1: 0.01,
+				0.5: 0.05,
+				0.9: 0.01,
+			},
+		}),
+		highwatermark: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "highwatermark",
+			Help:      "Current highwatermark of a topic-partition",
+		}, []string{"topic", "partition"}),
+		offset: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "offset",
+			Help:      "Current offset of a topic-partition",
+		}, []string{"topic", "partition"}),
+		samplesTotal: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "samples_total",
+			Help:      "count of samples ingested into compressor",
+		}, []string{"topic", "partition"}),
+		writeDuration: prometheus.NewSummary(prometheus.SummaryOpts{
+			Namespace: "vulcan",
+			Subsystem: "compressor",
+			Name:      "write_duration_seconds",
+			Help:      "Summary of durations taken writing a chunk to the database",
+		}),
+	}, nil
+}
+
+// Describe implements prometheus.Collector.
+func (m *Manager) Describe(ch chan<- *prometheus.Desc) {
+	m.fetchDuration.Describe(ch)
+	m.flushUtilization.Describe(ch)
+	m.highwatermark.Describe(ch)
+	m.offset.Describe(ch)
+	m.samplesTotal.Describe(ch)
+	m.writeDuration.Describe(ch)
+}
+
+// Collect implements prometheus.Collector.
+func (m *Manager) Collect(ch chan<- prometheus.Metric) {
+	m.fetchDuration.Collect(ch)
+	m.flushUtilization.Collect(ch)
+	m.highwatermark.Collect(ch)
+	m.offset.Collect(ch)
+	m.samplesTotal.Collect(ch)
+	m.writeDuration.Collect(ch)
+}
+
+func (m *Manager) Run() error {
+	return m.cfg.Coordinator.Run(m.handle)
+}
+
+func (m *Manager) handle(ctx context.Context, topic string, partition int32) {
+	if partition != 2 {
+		return
+	}
+	log := logrus.WithFields(logrus.Fields{
+		"topic":     topic,
+		"partition": partition,
+	})
+	log.Info("taking control of topic-partition")
+	count := 0
+	backoff := time.NewTimer(time.Duration(0))
+	for {
+		count++
+		select {
+		case <-ctx.Done():
+			return
+		case <-backoff.C:
+			c := NewCompressor(&CompressorConfig{
+				Context:   ctx,
+				Topic:     topic,
+				Partition: partition,
+			})
+			err := c.consume(ctx, topic, partition)
+			if err == nil {
+				logrus.WithFields(logrus.Fields{
+					"topic":     topic,
+					"partition": partition,
+				}).Info("relenquishing control of topic-partition")
+				return
+			}
+			panic(err)
+			// exponential backoff with cap at 10m
+			dur := time.Duration(math.Min(float64(time.Minute*10), float64(100*time.Millisecond)*math.Pow(float64(2), float64(count))))
+			logrus.WithFields(logrus.Fields{
+				"backoff_duration": dur,
+				"topic":            topic,
+				"partition":        partition,
+			}).WithError(err).Error("error while consuming but restarting after backoff")
+			backoff.Reset(dur)
+		}
+	}
+}
